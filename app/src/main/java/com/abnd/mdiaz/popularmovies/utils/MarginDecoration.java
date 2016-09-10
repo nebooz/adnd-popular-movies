2648d133c8d2ef20c798a0d3200702e10fb28a83
Maximiliano Diaz <nebooz@gmail.com>
2016-09-10 01:47:56 -0300

    A small fix to upper and lower borders in the card list

--- app/src/main/java/com/abnd/mdiaz/popularmovies/utils/MarginDecoration.java
+++ app/src/main/java/com/abnd/mdiaz/popularmovies/utils/MarginDecoration.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.graphics.Rect;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 
 import com.abnd.mdiaz.popularmovies.R;
@@ -14,11 +13,9 @@ public class MarginDecoration extends RecyclerView.ItemDecoration {
     private static final String TAG = MarginDecoration.class.getSimpleName();
 
     private Context mContext;
-    private int mCounter;
 
     public MarginDecoration(Context context) {
         mContext = context;
-        mCounter = 1;
     }
 
     @Override
@@ -29,28 +26,39 @@ public class MarginDecoration extends RecyclerView.ItemDecoration {
         This took way too much work to figure out... I mean, if anyone is using a GridLayoutManager...
         is it really that hard to add a bool parameter to center elements in the spans?
         */
+
         int cardWidth = mContext.getResources().getDimensionPixelSize(R.dimen.movie_list_card_width);
         int managerWidth = parent.getLayoutManager().getWidth();
         GridLayoutManager manager = (GridLayoutManager) parent.getLayoutManager();
         int managerSpans = manager.getSpanCount();
-        int recyclerWidth = parent.getWidth();
 
         int availableSpace = managerWidth - (cardWidth * managerSpans);
         int mTrueMargin = availableSpace / (managerSpans * 2);
+        int cardPosition = parent.getChildAdapterPosition(view);
 
-        Log.d(TAG, "getItemOffsets - ManagerWidth: " + String.valueOf(managerWidth) + " - RecyclerWidth: " + String.valueOf(recyclerWidth + " - Span Count: " + String.valueOf(managerSpans)));
-        Log.d(TAG, "getItemOffsets: TrueMargin: " + String.valueOf(mTrueMargin));
+        double itemCount = parent.getAdapter().getItemCount();
 
-        if (mCounter > managerSpans) {
+        double bottomCalculation = itemCount / managerSpans;
+        double fractionalPart = bottomCalculation % 1;
+        double integralPart = bottomCalculation - fractionalPart;
+
+        if (fractionalPart == 0 && ((cardPosition + 1) > (managerSpans * (integralPart - 1)))) {
+
+            outRect.set(mTrueMargin, mTrueMargin / 2, mTrueMargin, mTrueMargin);
+
+        } else if (fractionalPart > 0 && ((cardPosition + 1) > managerSpans * integralPart)) {
+
+            outRect.set(mTrueMargin, mTrueMargin / 2, mTrueMargin, mTrueMargin);
+
+        } else if (cardPosition + 1 > managerSpans) {
 
             outRect.set(mTrueMargin, mTrueMargin / 2, mTrueMargin, mTrueMargin / 2);
 
         } else {
 
             outRect.set(mTrueMargin, mTrueMargin, mTrueMargin, mTrueMargin / 2);
-        }
 
-        mCounter++;
+        }
 
     }
 }
