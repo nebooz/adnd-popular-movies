3f206ba164b0cd278caa5fb0de6d9e7ade5ce8b2
Maximiliano Diaz <nebooz@gmail.com>
2016-09-10 14:42:09 -0300

    Theme change and tiled background for movie list

--- app/src/main/java/com/abnd/mdiaz/popularmovies/MovieListActivity.java
+++ app/src/main/java/com/abnd/mdiaz/popularmovies/MovieListActivity.java
@@ -1,12 +1,17 @@
 package com.abnd.mdiaz.popularmovies;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 
@@ -18,6 +23,13 @@ public class MovieListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_list);
+
+        RelativeLayout activityBaseLayout = (RelativeLayout) findViewById(R.id.movie_list_fragment_container);
+
+        Bitmap bmp = BitmapFactory.decodeResource(getResources(), R.drawable.list_mini_dark);
+        BitmapDrawable bitmapDrawable = new BitmapDrawable(getResources(), bmp);
+        bitmapDrawable.setTileModeXY(Shader.TileMode.REPEAT, Shader.TileMode.REPEAT);
+        activityBaseLayout.setBackground(bitmapDrawable);
 
         if (!checkConnectivity()) {
 
